1ddd738ee29fa77d02886a4ca24caa60091e1122
liuxiyang <liuxiyang@jd.com>
2015-07-31 15:23:06 +0800

    create a new branch

--- main.js
+++ main.js
@@ -8,4 +8,7 @@ requirejs(['jquery', 'validata'],function($, validata){
 	console.log('something change');
 	console.log('我又更改了....');
 	console.log('track change....');
+
+
+	console.log('I am a new branch');
 });
